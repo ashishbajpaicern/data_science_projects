421f64953303ceaeb852eef1b4f81f2052ec39ab
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2022-08-10 21:28:32 +0530

    practice commit

--- data science practice/practice.py
+++ data science practice/practice.py
@@ -4,6 +4,7 @@ import numpy as np
 os. chdir('data science practice')
 mixer_data = pd.read_csv('Bosch Pro 1000W.csv')
 mixer_data1 = mixer_data.copy(deep=True)
+
 print(mixer_data1.size)
 print(mixer_data1.index)
 print(mixer_data1.columns)
