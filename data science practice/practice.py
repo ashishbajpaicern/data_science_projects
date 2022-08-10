19be8450fbd6515330db2128f74bdee5f6e81783
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2022-08-10 21:11:51 +0530

    trial

---
+++ data science practice/practice.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+import numpy as np
+os. chdir('data science practice')
+mixer_data = pd.read_csv('Bosch Pro 1000W.csv')
+mixer_data1 = mixer_data.copy(deep=True)
+print(mixer_data1.size)
+print(mixer_data1.index)
+print(mixer_data1.columns)
+print(mixer_data1.shape)
+print(mixer_data1.memory_usage())
+print(mixer_data1.ndim)
